41003ea1014bffcc02d61c4600e2835cafdf03ee
Ricardo <ricardoleonardo097@gmail.com>
2024-06-04 01:57:58 +0200

    first-commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import "./main.js";
+
+const boton = document.querySelector("#btn-buscar");
+
+const buscarGif = () => {
+  let resp = document.getElementById("txt-buscar").ariaValueMax.toUpperCase;
+  return resp;
+};
+
+boton.addEventListener("click", () => {});
