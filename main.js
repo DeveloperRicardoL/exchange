282946338dfab15278107814a614d19472e62880
Ricardo <ricardoleonardo097@gmail.com>
2024-06-05 23:14:57 +0200

    A graphic is implemented

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
 import "./style.css";
+import { Chart } from "chart.js/auto";
 
 const boton = document.querySelector("#BtnConvertir");
 const resp = document.querySelector("#LblMostrar");
+
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
 
 const montoIngresado = () => {
   let monto = document.getElementById("TxtIngreso").value;
