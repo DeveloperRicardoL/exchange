33c83d499cac8e769c8407e2123ae655af835d5d
Ricardo <ricardoleonardo097@gmail.com>
2024-06-05 18:39:20 +0200

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-import "./main.js";
+import "./style.css";
+
+const boton = document.querySelector("#BtnConvertir");
+const resp = document.querySelector("#LblMostrar");
+
+const montoIngresado = () => {
+  let monto = document.getElementById("TxtIngreso").value;
+  return monto;
+};
+
+boton.addEventListener("click", () => {
+  resp.innerHTML = `Sus ${montoIngresado()} pesos Argentinos son iguales a ${
+    montoIngresado() * 2
+  } Euros`;
+  document.getElementById("TxtIngreso").value = "";
+});
