9923caa3673f343166c33916fee830cd3be81429
Ricardo <ricardoleonardo097@gmail.com>
2024-06-05 17:20:59 +0200

    se agrega vite.config.js e imagen de icono

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
 import "./main.js";
-
-const boton = document.querySelector("#btn-buscar");
-
-const buscarGif = () => {
-  let resp = document.getElementById("txt-buscar").ariaValueMax.toUpperCase;
-  return resp;
-};
-
-boton.addEventListener("click", () => {});
